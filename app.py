f26a79f9f4a219f07ebf93a5583d2d8d6115a050
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-09-04 21:51:32 +0000

    initial upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from shiny import App, Inputs, Outputs, Session, render, ui
+
+import shinyswatch
+
+app_ui = ui.page_fluid(
+    shinyswatch.theme.darkly(),
+    ui.h2("Mortgage Calculator"),
+    ui.input_slider("price", "Home Price", 200000, 400000, 500),
+    ui.input_slider("downpayment", "Down Payment %", 10, 20, 1),
+    ui.input_slider("term", "Term in Years", 15, 30, 15),
+    ui.input_slider("hoa", "HOA", 200, 400, 50),
+    ui.output_text_verbatim("monthly"),
+    ui.h3("What could this likely rent for?"),
+    ui.input_slider("rent", "Monthly Rent", 1100, 1800, 100),
+)
+
+
+def server(input, output, session):
+    @output
+    @render.text
+    def monthly():
+        downpay_total = input.price() * (input.downpayment()/100)
+        annual_mortgage = (input.price() - downpay_total)/input.term()
+        monthly_mortgage = (annual_mortgage/12) + input.hoa()
+        return f"Monthly Mortgage is $ {monthly_mortgage}"
+
+
+app = App(app_ui, server)
